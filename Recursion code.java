f7654cfbff39b63ad99e1a47455e2bce9d48c4a3
Yashvanth0557 <yashvanth9783@gmail.com>
2026-07-17 15:03:20 +0530

    Create Recursion code.java

---
+++ Recursion code.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class main{
+    public static void main(String[]args){
+        int n=5;
+        pat(5);
+    }
+    static void pat(int k)
+    {
+        if(k==0)
+        {
+            return;
+        }
+        System.out.println(k);
+        k=k-1;
+        pat(k);
+        
+    }
+}
