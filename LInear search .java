80dcfd7f650f1d55414095fe5847526101bbb8aa
Yashvanth0557 <yashvanth9783@gmail.com>
2026-07-20 21:58:07 +0530

    Create LInear search .java

---
+++ LInear search .java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ArraySearch {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] name = {"anbu", "aathi", "abi"};
+
+        System.out.print("Enter the name: ");
+        String n = sc.nextLine();
+
+        for (int i = 0; i < name.length; i++) {
+            if (name[i].equals(n)) {
+                System.out.println("Name found");
+                return;
+            }
+        }
+
+        System.out.println("Name not found");
+
+        sc.close();
+    }
+}
