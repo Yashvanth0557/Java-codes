eb925ba6b58de29bdbf3a69cd36e8189c055dc74
Yashvanth0557 <yashvanth9783@gmail.com>
2026-07-21 14:09:21 +0530

    Create Dimond pattern code.java

---
+++ Dimond pattern code.java
@@ -0,0 +1,26 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+
+        // Upper part
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 3; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower part
+        for (int i = 2; i >= 1; i--) {
+            for (int j = 3; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
