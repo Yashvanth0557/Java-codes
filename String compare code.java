2d21ebd88332fe8e9605c805fb1e50b1e58df702
Yashvanthkuamr.R.G <yashvanth9783@gmail.com>
2026-07-31 15:30:17 +0530

    Create String compare code.java

---
+++ String compare code.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        String str1 = "YASHVANTHKUMAR";
+        String str2 = "YASHVANTHKUMAR";
+        System.out.println(str1.equals(str2));
+    }
+}
